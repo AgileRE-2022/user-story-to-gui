dbfe5f53aaa982b1825c0532eb4a3727d1296269
auraaksha <101512240+auraaksha@users.noreply.github.com>
2022-06-10 19:53:24 +0700

    Add files via upload

---
+++ converter/main/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import UserStory
+
+admin.site.register(UserStory)
---
+++ converter/main/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from .forms import UserStoryForm
+
+from .models import UserStory
+
+#from xxxx import handle_uploaded_file masih imaginary
+
+#def upload_file(request):
+    #if request.method == 'POST':
+	 #   form = UploadFileForm(request.POST, request.FILES)
+	  #  if form.is_valid():
+	   #    handle_uploaded_file(request.FILES)
+		#   return HttpResponseRedirect('/success/url/')
+	    #else:
+	     #  form = UploadFileForm()
+	    #return render(request, 'upload.html', {'form': form})
+
+def index(request):
+	#return HttpResponse("User Story to GUI Converter Main Page")
+	#USobjects = UserStory.objects.all
+	return render(request, 'index.html', {})
+	
+def newstory(request) :
+	if request.method == 'POST':
+		form = UserStoryForm(request.POST or None)
+		if form.is_valid()
+			form.save()
+		return render(request, 'index.html', {})
+	else:
+	return render(request, 'index.html', {})
+	
+
