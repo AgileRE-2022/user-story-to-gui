d109cf80e9907d7f378bfee04bdec68dc7d3c82f
alifmh <101513316+alifmh@users.noreply.github.com>
2022-06-24 12:14:08 +0700

    Delete converter directory

--- converter/main/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import UserStory
-
-admin.site.register(UserStory)
--- converter/main/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import UserStory
-
-class UserStoryForm(forms.Modelform):
-	class Meta:
-		model = UserStory
-		fields = ['role','task','benefit','initState','action','result']
